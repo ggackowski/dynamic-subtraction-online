a8b32d0777adae8a893ae05faead6b704f1595a1
Grzegorz Gackowski <305297@student.agh.edu.pl>
2021-05-13 20:03:49 +0200

    build

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDY2-jHQBZIXs-rKr9B50xc-LtUcW30_Mc",
+    authDomain: "dynamic-subtraction-online.firebaseapp.com",
+    projectId: "dynamic-subtraction-online",
+    storageBucket: "dynamic-subtraction-online.appspot.com",
+    messagingSenderId: "105581498015",
+    appId: "1:105581498015:web:c19443670b8993428ddcaa",
+    measurementId: "G-06SBHCW0RH"
+  }
 };
